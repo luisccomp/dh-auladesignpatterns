161408ad307e239866546aeddfea0bd8095d9e6a
Silas Vergilio <silas.vergilio@gmail.com>
2020-09-18 21:50:40 -0300

    hamburguer suino e misto

---
+++ src/com/aula/dh/model/HamburguerMisto.java
@@ -0,0 +1,9 @@
+package com.aula.dh.model;
+
+public  class HamburguerMisto extends Hamburguer {
+
+	public HamburguerMisto( int quantidade, String outros) {
+		super(TipoCarne.MISTO, quantidade, outros);
+	}
+	
+}
---
+++ src/com/aula/dh/model/HamburguerSuino.java
@@ -0,0 +1,9 @@
+package com.aula.dh.model;
+
+public  class HamburguerSuino extends Hamburguer {
+
+	public HamburguerSuino( int quantidade, String outros) {
+		super(TipoCarne.SUINO, quantidade, outros);
+	}
+	
+}
